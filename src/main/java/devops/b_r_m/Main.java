b33a89cc56f87bda9026d1dd08a1a904d10018fa
Hammad <hammadsiddiqui1212@gmail.com>
2023-03-25 15:51:01 +0530

    all files updated

--- src/main/java/devops/b_r_m/Main.java
+++ src/main/java/devops/b_r_m/Main.java
@@ -4,22 +4,22 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public void add(double a, double b)   {
-        System.out.println("Sum of"+a+"and"+b+"="+(a+b));
+        System.out.println("Sum of "+a+" and "+b+" ="+(a+b));
     }
     public void diff(double a, double b)   {
         if(a>b)
-            System.out.println("Difference of"+a+"and"+b+"="+(a-b));
+            System.out.println("Difference of "+a+" and "+b+"= "+(a-b));
         else
-            System.out.println("Difference of"+a+"and"+b+"="+(b-a));
+            System.out.println("Difference of "+a+" and "+b+"= "+(b-a));
     }
     public void mul(double a, double b)   {
-        System.out.println("Product of"+a+"with"+b+"="+a*b);
+        System.out.println("Product of "+a+" with "+b+" ="+a*b);
     }
     public void div(double a, double b)   {
         if(a==0 || b==0)
             System.out.println("Division not possible");
         else
-            System.out.println("Division of"+a+"/"+b+"="+a/b);
+            System.out.println("Division of "+a+"/"+b+" ="+a/b);
     }
     public static void main(String[] args) {
         Main m=new Main();
